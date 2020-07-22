c8b03c951735459858e6041e1538396c61ce152b
Александра <kruglyakova.sasha@gmail.com>
2020-07-22 17:19:08 +0300

    Stylize inner pages

--- build/js/script.js
+++ build/js/script.js
@@ -2,15 +2,21 @@ let modalOverlay = document.querySelector('.page-main__modal-overlay');
 let buttonOrder = document.querySelector('.special-offers__order-button');
 let buttonCloseModal = document.querySelector('.modal__button-close');
 
-buttonOrder.onclick = function () {
-    modalOverlay.classList.add('page-main__modal-overlay--open');
-    document.body.classList.add('no-scroll');
-};
+if (buttonOrder) {
+    buttonOrder.onclick = function () {
+        if (!modalOverlay) return;
+        modalOverlay.classList.add('page-main__modal-overlay--open');
+        document.body.classList.add('no-scroll');
+    };
+}
 
-buttonCloseModal.onclick = function () {
-    modalOverlay.classList.remove('page-main__modal-overlay--open');
-    document.body.classList.remove('no-scroll');
-};
+if (buttonCloseModal) {
+    buttonCloseModal.onclick = function () {
+        if (!modalOverlay) return;
+        modalOverlay.classList.remove('page-main__modal-overlay--open');
+        document.body.classList.remove('no-scroll');
+    };
+}
 
 let nav = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
